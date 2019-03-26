58feabb1375075e55804c16fc60d7f7aa3225316
Shota Nozaki <emonkak@gmail.com>
2019-03-27 01:52:52 +0900

    Add default charset and collation detection

--- model/table.go
+++ model/table.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"strings"
+)
+
 // NewTable create a new table with the given name
 func NewTable(name string) Table {
 	return &table{
@@ -149,6 +153,18 @@ func (t *table) Normalize() (Table, bool) {
 	var clone bool
 	var additionalIndexes []Index
 	var columns []TableColumn
+	var defaultCharacterSet string
+	var defaultCollation string
+
+	for opt := range t.Options() {
+		switch (strings.ToUpper(opt.Key())) {
+		case "DEFAULT CHARACTER SET":
+			defaultCharacterSet = opt.Value()
+		case "DEFAULT COLLATE":
+			defaultCollation = opt.Value()
+		}
+	}
+
 	for col := range t.Columns() {
 		ncol, modified := col.Normalize()
 		if modified {
@@ -184,6 +200,27 @@ func (t *table) Normalize() (Table, bool) {
 			}
 			ncol = ncol.Clone()
 			ncol.SetUnique(false)
+		}
+
+		switch ncol.Type() {
+		case ColumnTypeChar, ColumnTypeVarChar, ColumnTypeTinyText, ColumnTypeText, ColumnTypeMediumText, ColumnTypeLongText:
+			if !ncol.HasCharacterSet() {
+				if defaultCharacterSet != "" {
+					ncol.SetCharacterSet(defaultCharacterSet)
+				}
+			}
+
+			if !ncol.HasCollation() {
+				if ncol.HasCharacterSet() {
+					if ncol.CharacterSet() == defaultCharacterSet && defaultCollation != "" {
+						ncol.SetCollation(defaultCollation)
+					} else if collation := getDefaultCollationForCharacterSet(ncol.CharacterSet()); collation != "" {
+						ncol.SetCollation(collation)
+					}
+				} else if defaultCollation != "" {
+					ncol.SetCollation(defaultCollation)
+				}
+			}
 		}
 
 		columns = append(columns, ncol)
@@ -239,3 +276,92 @@ func (t *tableopt) ID() string       { return "tableopt#" + t.key }
 func (t *tableopt) Key() string      { return t.key }
 func (t *tableopt) Value() string    { return t.value }
 func (t *tableopt) NeedQuotes() bool { return t.needQuotes }
+
+func getDefaultCollationForCharacterSet(characterSet string) string {
+	switch characterSet {
+	case "big5":
+		return "big5_chinese_ci"
+	case "dec8":
+		return "dec8_swedish_ci"
+	case "cp850":
+		return "cp850_general_ci"
+	case "hp8":
+		return "hp8_english_ci"
+	case "koi8r":
+		return "koi8r_general_ci"
+	case "latin1":
+		return "latin1_swedish_ci"
+	case "latin2":
+		return "latin2_general_ci"
+	case "swe7":
+		return "swe7_swedish_ci"
+	case "ascii":
+		return "ascii_general_ci"
+	case "ujis":
+		return "ujis_japanese_ci"
+	case "sjis":
+		return "sjis_japanese_ci"
+	case "hebrew":
+		return "hebrew_general_ci"
+	case "tis620":
+		return "tis620_thai_ci"
+	case "euckr":
+		return "euckr_korean_ci"
+	case "koi8u":
+		return "koi8u_general_ci"
+	case "gb2312":
+		return "gb2312_chinese_ci"
+	case "greek":
+		return "greek_general_ci"
+	case "cp1250":
+		return "cp1250_general_ci"
+	case "gbk":
+		return "gbk_chinese_ci"
+	case "latin5":
+		return "latin5_turkish_ci"
+	case "armscii8":
+		return "armscii8_general_ci"
+	case "utf8":
+		return "utf8_general_ci"
+	case "ucs2":
+		return "ucs2_general_ci"
+	case "cp866":
+		return "cp866_general_ci"
+	case "keybcs2":
+		return "keybcs2_general_ci"
+	case "macce":
+		return "macce_general_ci"
+	case "macroman":
+		return "macroman_general_ci"
+	case "cp852":
+		return "cp852_general_ci"
+	case "latin7":
+		return "latin7_general_ci"
+	case "utf8mb4":
+		return "utf8mb4_general_ci"
+	case "cp1251":
+		return "cp1251_general_ci"
+	case "utf16":
+		return "utf16_general_ci"
+	case "utf16le":
+		return "utf16le_general_ci"
+	case "cp1256":
+		return "cp1256_general_ci"
+	case "cp1257":
+		return "cp1257_general_ci"
+	case "utf32":
+		return "utf32_general_ci"
+	case "binary":
+		return "binary"
+	case "geostd8":
+		return "geostd8_general_ci"
+	case "cp932":
+		return "cp932_japanese_ci"
+	case "eucjpms":
+		return "eucjpms_japanese_ci"
+	case "gb18030":
+		return "gb18030_chinese_ci"
+	default:
+		return ""
+	}
+}
